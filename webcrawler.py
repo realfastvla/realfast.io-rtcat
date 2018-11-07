895101a38355bf1e27c4c7e9ef657be99fec1970
Casey Law <caseyjlaw@gmail.com>
2018-11-07 14:34:21 -0800

    fix for catalog changes

--- webcrawler.py
+++ webcrawler.py
@@ -2,31 +2,36 @@ import requests
 import re
 
 def get_ATNF_version():
-	r = requests.get("http://www.atnf.csiro.au/people/pulsar/psrcat/")
-	p = re.compile('name="version" value="([^"]+)')
-	m = p.search(r.text)
-	return m.group(1)
+    r = requests.get("http://www.atnf.csiro.au/people/pulsar/psrcat/")
+    p = re.compile('name="version" value="([^"]+)')
+    m = p.search(r.text)
+    if m is not None:
+        return m.group(1)
 
 def get_RRATalog_version():
-	r = requests.get("http://astro.phys.wvu.edu/rratalog/")
-	p = re.compile('Last update: (.+)')
-	m = p.search(r.text)
-	return m.group(1).strip()
+    r = requests.get("http://astro.phys.wvu.edu/rratalog/")
+    p = re.compile('Last update: (.+)')
+    m = p.search(r.text)
+    if m is not None:
+        return m.group(1).strip()
 
 def get_Parallaxes_version():
-	r = requests.get("http://www.astro.cornell.edu/research/parallax/")
-	p = re.compile('All published pulsar parallaxes as of <b>(.+)</b>')
-	m = p.search(r.text)
-	return m.group(1).strip()
+    r = requests.get("http://www.astro.cornell.edu/research/parallax/")
+    p = re.compile('All published pulsar parallaxes as of <b>(.+)</b>')
+    m = p.search(r.text)
+    if m is not None:
+        return m.group(1).strip()
 
 def get_GCpsr_version():
-	r = requests.get("http://www.naic.edu/~pfreire/GCpsr.html")
-	p = re.compile('<i> The present version of this page was last updated: (.+)</i>')
-	m = p.search(r.text)
-	return m.group(1).strip()
+    r = requests.get("http://www.naic.edu/~pfreire/GCpsr.html")
+    p = re.compile('This page was last updated: (.+). In')
+    m = p.search(r.text)
+    if m is not None:
+        return m.group(1).strip()
 
 def get_frbcat_version():
-	r = requests.get("http://www.astronomy.swin.edu.au/pulsar/frbcat/")
-	p = re.compile('<h2>Catalogue Version (.+)</h2>')
-	m = p.search(r.text)
-	return m.group(1).strip()
+    r = requests.get("http://frbcat.org")
+    p = re.compile('<h2>Catalogue Version (.+)</h2>')
+    m = p.search(r.text)
+    if m is not None:
+        return m.group(1).strip()
